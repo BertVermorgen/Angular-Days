c171bcf973be02f41e1dbc4a5112d786eda35f71
Bert Vermorgen <bert.vermorgen@athumi.be>
2024-05-31 13:56:17 +0200

    Update templates to v17 template syntax

--- itenium-socks/src/app/socks/shop.component.ts
+++ itenium-socks/src/app/socks/shop.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { SocksService } from './socks.service';
 import { Observable } from 'rxjs';
 import { Sock } from './sock.model';
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [NgFor, AsyncPipe],
+  imports: [AsyncPipe],
   templateUrl: './shop.component.html'
 })
 export class ShopComponent {
