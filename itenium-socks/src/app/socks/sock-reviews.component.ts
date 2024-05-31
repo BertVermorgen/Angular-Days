d01a22ee4525eb77eb2e37d965a78ce3bb4cbd22
Bert Vermorgen <bert.vermorgen@athumi.be>
2024-05-31 14:33:44 +0200

    Display review date as time ago

--- itenium-socks/src/app/socks/sock-reviews.component.ts
+++ itenium-socks/src/app/socks/sock-reviews.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SocksService } from '@services/socks.service';
 import { Review } from '@models/sock.model';
+import {SockReviewComponent} from "@app/socks/sock-review/sock-review.component";
 
 @Component({
   selector: 'app-sock-reviews',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, SockReviewComponent],
   templateUrl: './sock-reviews.component.html'
 })
 export class SockReviewsComponent {
