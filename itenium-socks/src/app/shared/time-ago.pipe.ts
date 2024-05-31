e8feb18067b17fc2a6ba309f47f52845c7fa2e80
Bert Vermorgen <bert.vermorgen@athumi.be>
2024-05-31 14:36:36 +0200

    Auto update time ago

--- itenium-socks/src/app/shared/time-ago.pipe.ts
+++ itenium-socks/src/app/shared/time-ago.pipe.ts
@@ -6,7 +6,40 @@ import { formatDistanceToNow } from 'date-fns';
   standalone: true
 })
 export class TimeAgoPipe implements PipeTransform {
+  private lastValue: any;
+  private lastText: string;
+  private timer: any;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef, private ngZone: NgZone) {}
+
   transform(value: Date | string | number): string {
+    if (this.lastValue !== value) {
+      this.lastValue = value;
+      this.lastText = this.formatTimeAgo(value);
+      this.resetTimer();
+    }
+    return this.lastText;
+  }
+
+  private formatTimeAgo(value: Date | string | number): string {
     return formatDistanceToNow(new Date(value), { addSuffix: true });
+  }
+
+  private resetTimer() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    this.ngZone.runOutsideAngular(() => {
+      this.timer = setTimeout(() => {
+        this.lastText = this.formatTimeAgo(this.lastValue);
+        this.ngZone.run(() => this.changeDetectorRef.markForCheck());
+      }, 60000); // Update every minute
+    });
+  }
+
+  private cleanup() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
   }
 }
